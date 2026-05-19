78aa3df0c8a38cf9bb78acbd4fc436ac2e14454a
AndreeVillegas <andree.w.villegas@gmail.com>
2026-05-19 16:47:55 -0500

    Fronted Avance 01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
     implementation(libs.okhttp)
+    implementation(libs.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
